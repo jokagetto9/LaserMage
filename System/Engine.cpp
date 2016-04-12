046cb074f0afeab72b4fd6fd136f9017d7223c39
jokagetto9 <jokagetto9@gmail.com>
2016-04-11 18:17:20 -0600

    Restored ClockCycle
    Added ClockCycle for accumulating cycle lag
    phys (20-40fps) and rapid (1-16fps) lag

--- System/Engine.cpp
+++ System/Engine.cpp
@@ -1,8 +1,7 @@
 //********************************* INITIALIZATION *********************************
 #include "Engine.h"
 
-Engine::	Engine(){
-	
+Engine::	Engine(){	
 		menuView = false;
 }
 
@@ -21,10 +20,10 @@ void Engine::		loadingMenu(){
 }
 
 void Engine::quit(){
-
 	//world.quit();
 	//if(_DEBUG) cout << "Zones Deleted" << endl;
 }
+
 
 //********************************* PRIMARY CYCLES *********************************
 void Engine::update(){		
@@ -35,17 +34,72 @@ void Engine::update(){
 	//DBT->update();
 	//if (G->save) save();
 	//if (G->skip == 0){ 
-	//	clockCycle();
+		clockCycle();
 	//} else skipTime();
 	
 	//stack.update(input);
 	//input.clearKeys();	
 }
 
-//*/
+
+
+
+
+
+
+//*/	
+
+
+
+void Engine::clockCycle(){
+	G->trackAVG(); G->trackFPS();	
+	if (!G->paused){
+		G->trackTime();
+		//world.hourlyUpdate();
+	}
+	G->incLag();
+	while (G->testLag()){	// while phys delta has time lag
+		if(G->lagVSlag())	physicsUpdate();	// phys has more lag			
+		else				rapidUpdate();		// if ai has equal or more lag			
+	}		
+	G->prevTime = G->curTime;	// reset cycle
+	 //else G->action = false;
+}
+
+void Engine::		physicsUpdate(){
+	if (!G->paused){	
+		//input.cameraInput();
+		//world.physUpdate();
+		//world.interactions();
+		//DBT->physUpdate(); //?
+	}
+	G->action = false;
+	G->decPhysLag();		
+}
+
+void Engine::		rapidUpdate(){
+	if (!G->paused){	
+		//world.rapidUpdate();
+		if (G->frequently) {
+			//world.freqUpdate();
+			G->frequently = false;
+		}
+	}
+	//stack.rapidUpdate();
+	G->decAILag();
+}
+	//if (G->slowCount == SLOW_AI_FACTOR-1) freqUpdate();// slow ai runs by a factor of ai cycles
+
+
+
+
+
+
+
+
 //********************************* DRAW *********************************
 
-void Engine::display(){			
+void Engine::display(){	
 		glClearColor(1, 1, 1, 1);	
 		clearDisplay();
 		setUp2DView(RES.x, RES.z);
@@ -54,14 +108,15 @@ void Engine::display(){
 		M->menuBO.draw(0);
 		disable2DView();
 }
+
  
 void Engine::clearDisplay(){
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glMatrixMode(GL_MODELVIEW);	
 		glLoadIdentity();	// initialize the selected matrix
-
 }
+
 
 
 void Engine :: setUp2DView (int width, int height){
@@ -74,23 +129,23 @@ void Engine :: setUp2DView (int width, int height){
 		glLoadIdentity();
 		//gluOrtho2D(0.0, width, 0.0, width);
 		glOrtho(0.0, width, 0.0, height, 0, 1);
-
-
-
 		menuView = true;
 	}
 }
 
-void Engine :: disable2DView ()
-{
+void Engine :: disable2DView (){
 	if (menuView){
 		glMatrixMode(GL_PROJECTION);
 		glPopMatrix();
-
 		menuView = false;
 		//glMatrixMode(GL_MODELVIEW);
 		//glPopMatrix();
-
 	}
 }
+
+
+
+
+
+
 
